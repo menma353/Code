3a269dc8f5bb4199f843339a1287ee9e47ada8a2
menma <chinemenma6@gmail.com>
2025-03-13 08:17:09 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fcc-quote-machine"
+  base: process.env.VITE_BASE_PATH || "/freecodecamp-quote-machine"
 })
